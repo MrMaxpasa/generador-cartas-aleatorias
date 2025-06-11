a506c36935cd3944acb7db1608a47d3205f4f0c7
MrMaxpasa <mister.maxpasa@gmail.com>
2025-06-11 12:59:17 +0200

    cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+const suits = [
+  { symbol: '♥', color: 'red' },
+  { symbol: '♦', color: 'red' },
+  { symbol: '♣', color: 'black' },
+  { symbol: '♠', color: 'black' }
+];
+
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateCard() {
+  const value = getRandomItem(values);
+  const suit = getRandomItem(suits);
+  const elements = ['value-top', 'value-bottom', 'suit-top', 'suit-bottom', 'value-center'];
+
+  elements.forEach(id => {
+    const el = document.getElementById(id);
+      if (id.includes('value')) {
+        el.textContent = value;
+      } else {
+        el.textContent = suit.symbol;
+      }
+      el.className = suit.color === 'red' ? `${id} red` : id;
+  });
+}
+window.onload = generateCard;
